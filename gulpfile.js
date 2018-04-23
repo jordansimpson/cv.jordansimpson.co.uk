761374ec6cf156c0d8fece5c1c04601e11d88053
Jordan Simpson <jordan_simpson100@hotmail.co.uk>
2018-04-23 13:48:45 +0100

    intial refresh commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,75 @@
-// Include gulp
-var gulp = require('gulp'); 
+// Set site URL to be called in tasks
+var siteUrl = 'http://superdream.dev/superdream-boilerplate';
 
-// Include all required plugins
-var jshint = require('gulp-jshint'),
+// Include gulp and all required plugins
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
     sass = require('gulp-sass'),
-    minifyCSS = require('gulp-minify-css'),
+    cssnano = require('gulp-cssnano'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    browserSync = require('browser-sync');
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    uglify = require('gulp-uglify'),
+    notify = require('gulp-notify'),
+    imagemin = require('gulp-imagemin'),
+    stats = require('gulp-stats')(gulp);
 
-// Error Log Function to handle errors without disrupting watch
-function errorLog(error) {
-    console.error.bind(error);
-    this.emit('end');
-}
+// Browser Sync
+gulp.task('serve', ['sass'], function(){
+  browserSync.init({
+    proxy: siteUrl
+  });
+});
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('src/scripts/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
-
-// Initiate Browser Sync 
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "./"
-        }
-    });
+  return gulp.src('src/scripts/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('src/sass/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/css'))
-        .pipe(minifyCSS({keepBreaks:false}))
-        .pipe(rename('frontly.min.css'))
-        .pipe(gulp.dest('dist/css/min'))
-        .pipe(browserSync.reload({stream:true}));
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rename('style.min.css'))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css/min'))
+    .pipe(browserSync.stream())
+    .pipe(notify({ message: 'Styles task complete' }));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('src/scripts/*.js')
-        .pipe(concat('fronlty-scripts.js'))
-        .pipe(gulp.dest('dist/scripts'))
-        .pipe(uglify())
-        .pipe(rename('fronlty-scripts.min.js'))
-        .pipe(gulp.dest('dist/scripts/min'));
+  return gulp.src('src/scripts/*.js')
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(uglify())
+    .pipe(rename('scripts.min.js'))
+    .pipe(gulp.dest('dist/scripts/min'))
+    .pipe(browserSync.stream())
+    .pipe(notify({ message: 'Script task complete' }));
 });
 
+//Optimise Images
+gulp.task('images', () =>
+  gulp.src('src/images/*')
+      .pipe(imagemin())
+      .pipe(gulp.dest('dist/img'))
+);
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('src/sass/*.scss', ['sass', 'browser-sync']);
-    gulp.watch('src/scripts/*.js', ['lint', 'scripts']);
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+  gulp.watch('src/scripts/**/*.js', ['lint', 'scripts']);
 });
 
+
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'browser-sync', 'watch']);
+gulp.task('default', ['serve', 'images', 'lint', 'sass', 'scripts', 'watch']);
